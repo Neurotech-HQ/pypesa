8464717d2a4e1e89372fcb4ae08c035902d74a40
Kalebu Gwalugano <isaackeinstein@gmail.com>
2020-12-27 22:58:13 +0300

    added connectionerror exception

--- pypesa/mpesa_exceptions.py
+++ pypesa/mpesa_exceptions.py
@@ -39,3 +39,19 @@ class LoadingKeyError(Exception):
 
     def __init__(self, error_message=error_message):
         super().__init__(error_message)
+
+
+class ConnectionError(Exception):
+    """
+    This is exception will be thrown where there is no or slow internet connection
+    """
+
+    error_message = f"""
+
+    Transaction couldn\'t be processed due to connection timeout
+    
+    Please make sure you have a stable internet Connection 
+    """
+
+    def __init__(self, error_message=error_message):
+        super().__init__(error_message)
