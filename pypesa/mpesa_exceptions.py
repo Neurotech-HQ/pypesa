3f7cabd66b91a0c295f68d24a48dd360302d2793
Kalebu Gwalugano <isaackeinstein@gmail.com>
2020-12-27 22:37:25 +0300

    added LoadingKeyError exception

--- pypesa/mpesa_exceptions.py
+++ pypesa/mpesa_exceptions.py
@@ -1,25 +1,41 @@
+key_format = {"api_key": "xxxxxxxxx", "public_key": "xxxxxxx"}
+
 
 class AuthenticationError(Exception):
     """
-    Mpesa api Could'nt verify your keys 
+    Mpesa api Could'nt verify your keys
 
     Please try entering again carefully
 
     """
 
-    error_message = """
+    error_message = f"""
+        Could not verify your authentication keys
 
-    Could not verify your authentication keys
-    
-    keys.json should be formatted as shown below 
-    
-    {
-        'api_key' : 'xxxxxxxxx
-        'public_key' : 'xxxxxxx', 
-    }
+        keys.json should be formatted as shown below 
 
+            {key_format}
     """
-    
+
     def __init__(self, error_message=error_message):
         super().__init__(error_message)
 
+
+class LoadingKeyError(Exception):
+    """
+
+    Exception thrown while loading authentication keys
+
+    """
+
+    error_message = f"""
+
+        Exception thrown while loading authentication keys from file 
+
+        Please make sure you format correctly in json format as shown below 
+
+            {key_format}
+    """
+
+    def __init__(self, error_message=error_message):
+        super().__init__(error_message)
