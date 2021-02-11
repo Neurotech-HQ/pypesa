54bd659db34690670617ee84b8eba4c834861193
Kalebu Gwalugano <isaackeinstein@gmail.com>
2021-02-12 00:44:06 +0300

    added license.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,42 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-    name='pypesa',
-    version='0.0.1',
+    name="pypesa",
+    version="0.1",
     description='Python package for Vodacom Mpesa API Integration',
     url='https://github.com/Kalebu/pypesa',
+    download_url="https://github.com/Kalebu/pypesa/archive/0.1.tar.gz",
     author="Jordan Kalebu",
     author_email="isaackeinstein@gmail.com",
     license="MIT",
-    packages=['pypesa'],
+    packages=["pypesa"],
+    keywords=[
+        "pypesa",
+        "python-pesa",
+        "mpesa python",
+        "vodacom python",
+        "pypesa package",
+        "pypesa python package",
+        "vodacom python package",
+        "pypesa github",
+        "python pypesa",
+    ],
+
     install_requires=[
         'requests',
         'pycryptodome'
     ],
-    
-    python_requires='>=3.6'
+ 
+    include_package_data=True,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
